9f6da8a3aae4f25b76726fcb5b74c0e28dcc9d98
Jayden Ju (주재훈) <121030294+jaehunju1996@users.noreply.github.com>
2023-08-02 18:28:28 +0900

    23.08.02
    study

---
+++ jQuery/16 Floating banner/js/script.js
@@ -0,0 +1,30 @@
+$(function () {
+  var fnFloatBanner = function () {
+    var scry = $(window).scrollTop();
+    var wscry = $(window).scrollLeft();
+    var winh = $(window).height();
+    var winw = $(window).width();
+    $('.banner1').css({
+      top: scry + winh * 0.5 - 100,
+      left: wscry + 10,
+    });
+    $('.banner2').css({
+      top: scry + winh * 0.5 - 100,
+      left: wscry + winw * 0.5 - 50,
+    });
+    $('.banner3').css({
+      top: scry + winh * 0.5 - 100,
+      left: wscry + winw - 110,
+    });
+  }; //fnFloatBanner
+
+  fnFloatBanner();
+
+  $(window)
+    .scroll(function () {
+      fnFloatBanner();
+    })
+    .resize(function () {
+      fnFloatBanner();
+    }); //window event
+});
